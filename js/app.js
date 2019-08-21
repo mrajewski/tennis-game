7a83c31b22699df04abb369e1643938f9eec7c48
mrajewski <rajewski.ma@gmail.com>
2019-08-21 18:15:54 +0200

    computer paddle AI and scoreboard

--- js/app.js
+++ js/app.js
@@ -4,14 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const framesPerSecond = 30;
 
     let paddleY = 250;
+    let paddle2Y = 250;
     const paddleHeight = 100;
+    const paddleWidth = 10;
 
     let ballX = 50;
     let ballSpeedX = 10;
 
     let ballY = 50;
     let ballSpeedY = 4;
-    
+
+    let player1Score = 0;
+    let player2Score = 0;
+
 
 
     // paddle control by mouse
@@ -21,15 +26,15 @@ document.addEventListener("DOMContentLoaded", function () {
         let mouseX = evt.clientX - rect.left - root.scrollLeft;
         let mouseY = evt.clientY - rect.top - root.scrollTop;
         return {
-            x:mouseX,
-            y:mouseY
+            x: mouseX,
+            y: mouseY
         };
 
     }
 
-    canvas.addEventListener('mousemove',function (evt) {
+    canvas.addEventListener('mousemove', function (evt) {
         const mouse = mousePos(evt);
-        paddleY = mouse.y - (paddleHeight/2);
+        paddleY = mouse.y - (paddleHeight / 2);
 
 
     });
@@ -38,23 +43,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function ballReset() {
         ballSpeedX = -ballSpeedX;
-        ballX = canvas.width/2;
-        ballY = canvas.height/2;
+        ballX = canvas.width / 2;
+        ballY = canvas.height / 2;
 
     }
 
+    function computerAI() {
+        const paddle2YCenter = paddle2Y + (paddleHeight/2);
+        if (paddle2YCenter < ballY-35) {
+            paddle2Y +=6;
+
+        } else if (paddle2YCenter > ballY +35 ){
+            paddle2Y -= 6;
+        }
+    }
+
     function moveAll() {
+        computerAI();
+
         ballX += ballSpeedX;
         ballY += ballSpeedY;
         if (ballX > canvas.width) {
-            ballSpeedX = -ballSpeedX;
+            if (ballY > paddle2Y && ballY < paddle2Y + paddleHeight) {
+                ballSpeedX = -ballSpeedX;
+            } else{
+                ballReset();
+                player1Score ++;
+            }
 
         } else if (ballX < 0) {
-            if (ballY>paddleY && ballY< paddleY + paddleHeight){
+            if (ballY > paddleY && ballY < paddleY + paddleHeight) {
                 ballSpeedX = -ballSpeedX;
-            }else {
-                ballReset()
-
+            } else {
+                ballReset();
+                player2Score ++;
             }
         } else if (ballY < 0) {
             ballSpeedY = -ballSpeedY;
@@ -85,10 +107,16 @@ document.addEventListener("DOMContentLoaded", function () {
         rect(0, 0, canvas.width, canvas.height, 'black');
 
         //left player paddle
-        rect(0, paddleY, 10, paddleHeight, 'white');
+        rect(0, paddleY, paddleWidth, paddleHeight, 'white');
 
+        //computer  paddle
+        rect(canvas.width - paddleWidth, paddle2Y, paddleWidth, paddleHeight, 'white');
         //ball
         circle(ballX, ballY, 10, 'white');
+
+
+        canvasContext.fillText(`Score: ${player1Score}`,100,100);
+        canvasContext.fillText(`Score: ${player2Score}`,canvas.width - 100,100)
 
 
     }
